a4604fea500b3488c059b62e32d4db1a03be5237
Ali Bailoun <alibailoun1982@gmail.com>
2023-02-15 01:58:24 -0500

    added sequelize links

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { Category, Product, Tag, ProductTag } = require('./models');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
