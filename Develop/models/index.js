0e629ee40e8b73794646a7326d4f2fb9ca2377a7
Ali Bailoun <alibailoun1982@gmail.com>
2023-02-19 21:46:00 -0500

    routes all work, project is done

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,17 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id"
-})
+Product.belongsTo(Category)
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: ""
-})
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: true
+  },
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: true
+  }
+})
 
 module.exports = {
   Product,
